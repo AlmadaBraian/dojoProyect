62cca2c7788699d55182db0690bd05f5a11bcab5
Almada <braian.almada@capgemini.com>
2021-04-08 17:19:15 -0300

    se implementaron promesas, se agrego la tabla dinamica, se le agrego css

---
+++ src/main/resources/templates/js/tabla.js
@@ -0,0 +1,5 @@
+dojo.require("dijit.dijit");
+dojo.require("dojox.layout.TableContainer");
+dojo.require("dijit.form.TextBox");
+dojo.require("dijit.form.CheckBox");
+dojo.require("dijit.form.NumberSpinner");
